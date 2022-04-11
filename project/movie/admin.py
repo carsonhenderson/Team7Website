feb193c34a3953fba103d6deda243ba40d58a1a6
Carson Henderson <carsonhenderson@unomaha.edu>
2022-04-11 14:55:05 -0500

    Added website

--- project/movie/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Movie, Genre, Review, Request, Profile
-
-admin.site.register(Movie)
-admin.site.register(Genre)
-admin.site.register(Review)
-admin.site.register(Request)
-admin.site.register(Profile)
